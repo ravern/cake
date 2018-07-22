6ad07992a0757ca2821fb33ac2df94b9920bbc73
Ravern Koh <ravernkoh@gmail.com>
2018-07-22 17:28:17 +0800

    Add support for multiple targets in same definition

--- src/cake/dsl.cr
+++ src/cake/dsl.cr
@@ -1,11 +1,14 @@
 require "process"
 
 module Cake::DSL
+  # Represents the name of a target or dependency.
+  alias Name = String | Symbol
+
   # Sets the given target to be the default target.
   #
   # If a default target is not set, it is automatically set to the first target
   # that was defined.
-  def default(name : String | Symbol)
+  def default(name : Name)
     Targets::INSTANCE.default = name.to_s
   end
 
@@ -13,18 +16,36 @@ module Cake::DSL
   #
   # Phony targets are targets that will always be rebuilt, no matter when their
   # files exist or when their dependencies are not rebuilt.
-  def phony(name : String | Symbol)
-    Targets::INSTANCE.phonies << name.to_s
+  def phony(name : Name | Array(Name))
+    case name
+    when Name
+      names = [name]
+    else
+      names = name
+    end
+
+    Targets::INSTANCE.phonies += names.map(&.to_s)
   end
 
   # Defines a new target.
-  def target(name : String | Symbol, deps : Array(String | Symbol) = [] of String | Symbol, desc : String = "", &build : Env ->)
-    Targets::INSTANCE.all[name.to_s] = Target.new(name.to_s, deps.map(&.to_s), desc, &build)
+  def target(name : Name | Array(Name), deps : Array(Name) = [] of Name, desc : String = "", &build : Env ->)
+    case name
+    when Name
+      names = [name]
+    else
+      names = name
+    end
+
+    names.each do |name|
+      name = name.to_s
+      deps = deps.map(&.to_s)
+      Targets::INSTANCE.all[name] = Target.new(name, deps, desc, &build)
+    end
   end
 
   # Runs an external command.
   #
-  # Raises a `BuildError` if an error occured while running. If the `quiet` flag
+  # Raises a `RunError` if an error occured while running. If the `quiet` flag
   # is set, the command that was run will not be displayed.
   def run(command : String, args = nil, env : Process::Env = nil, clear_env : Bool = false, shell : Bool = false, input : Bool = true, output : Bool = true, error : Bool = true, chdir : String? = nil, quiet : Bool = false)
     unless quiet
